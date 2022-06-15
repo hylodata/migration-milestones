9226b539de408130904d7168a9bbce8a7007dfc9
gbby <gabby.resch@utoronto.ca>
2022-06-15 19:16:10 -0400

    updated main story content

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/branigan/cjz37rcb003ib1cr3s8rnkt2d',
+    style: 'mapbox://styles/gbby/cl4g7cfhv001n14qxgenegeqn',
     accessToken: 'pk.eyJ1IjoiZ2JieSIsImEiOiJjbDRnMnhpcWowOTZ0M2ltYmVidmFlYzUyIn0.YMvE-Zv6jfxhACw69xOvLQ',
     showMarkers: false,
     theme: 'light',
@@ -7,7 +7,7 @@ var config = {
     title: "Patience's Journey",
     subtitle: '',
     byline: '',
-    footer: 'Source: Story text from Wikipedia, August 2019. Data from <a href="https://www.usgs.gov/centers/norock/science/retreat-glaciers-glacier-national-park">USGS</a>',
+    footer: 'Source: CERC Migration, 2022. Data from <a href="http://losingtime.ca">Toronto Metropolitan University</a>',
     chapters: [{
             id: 'patience',
             alignment: 'full',
@@ -18,41 +18,60 @@ var config = {
                 center: [0.47773, 7.15319],
                 zoom: 16.00,
                 pitch: 45.00,
-                bearing: -40.00
+                bearing: -40.00,
+	        speed: 0.2,
+                curve: 1		
             },
-            onChapterEnter: [{
-                    layer: 'gnpglaciers-1998',
-                    opacity: 0.25
-                },
-                {
-                    layer: 'glaciernp-boundary',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [{
-                layer: 'glaciernp-boundary',
-                opacity: 0
-            }]
+            // onChapterEnter: [],
+            // onChapterExit: [// {
+            // //     layer: 'glaciernp-boundary',
+            // //     opacity: 0
+            // // }
+	    // 		   ]
         },
         {
             id: 'hohoe2003',
             alignment: 'left',
-            title: 'Hohoe, Volta Region, Ghana 2003',
+            title: 'Hohoe, Volta Region, Ghana',
             image: '',
             description: "In 2003, Patience left Hohoe  Municipality in Ghana's Volta Region, Ghana. She traveled by bus to Lomé, Togo.",
+            // location: {
+            //     center: [0.47773, 7.15319],
+            //     zoom: 10.00,
+            //     pitch: 45.00,
+            //     bearing: -40.00,
+	    //     speed: 0.5,
+            //     curve: 1
+            // },
+            // onChapterEnter: [],
+            // onChapterExit: [
+            //     // {
+            //     //     layer: 'gnpglaciers-2015',
+            //     //     opacity: 0
+            //     // }
+            // ]
+        },
+	               {
+            id: 'lome2003',
+            alignment: 'left',
+            title: 'Lome, Togo',
+            image: '',
+            description: "The bus trip to Lomé took...",
             location: {
                 center: [1.22540, 6.13253],
-                zoom: 13.31,
+                zoom: 15.00,
                 pitch: 45.00,
-                bearing: 0.00
+                bearing: 0.00,
+	        speed: 0.5,
+                curve: 1		
             },
-            onChapterEnter: [],
-            onChapterExit: [
-                // {
-                //     layer: 'gnpglaciers-2015',
-                //     opacity: 0
-                // }
-            ]
+            // onChapterEnter: [],
+            // onChapterExit: [
+            //     // {
+            //     //     layer: 'gnpglaciers-2015',
+            //     //     opacity: 0
+            //     // }
+            // ]
         }
     ]
 }
