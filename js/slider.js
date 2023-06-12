81c89f84f1d31de162a48a207851522f48c7ef38
gbby <gabby.resch@utoronto.ca>
2023-06-12 17:49:25 -0400

    updated couple of css items

--- js/slider.js
+++ js/slider.js
@@ -7,20 +7,24 @@ let slider = tns({
     controls: false,
     autoplayButtonOutput: false,
     responsive: {
+	2048: {
+	    items: 6,
+	    gutter: 20
+	},
         1600: {
-            items: 6,
-            gutter: 20
-        },
-        1024: {
             items: 5,
             gutter: 20
         },
-        768: {
+        1024: {
             items: 4,
             gutter: 20
         },
+        768: {
+            items: 3,
+            gutter: 20
+        },
         480: {
-            items: 3
+            items: 2
         }
     }
 })
